b4984aa05895fb965224320ca0f8dbe7c5d179df
ElamparithiMarimuthu <139229333+ElamparithiMarimuthu@users.noreply.github.com>
2024-03-15 16:23:13 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'aws cicd sevices sucessfully implemented"
+    return " aws cicd sevices sucessfully implemented "
 
 if __name__ == '__main__':
     app.run()
